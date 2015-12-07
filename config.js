f1207ab5aca6b70282f8ed15e3074ac8a11299ff
Phuong H <phuonghqh@gmail.com>
2015-12-07 09:50:17 +0700

    Initialise project code

---
+++ config.js
@@ -0,0 +1,11 @@
+require.config( {
+    paths: {
+        text: "bower_components/text/text",
+        jquery: "bower_components/jQuery/dist/jquery",
+        'jquery.scrollbar': "bower_components/jquery.scrollbar/jquery.scrollbar",
+        ractive: "bower_components/ractive/ractive",
+        'amd-loader': "bower_components/rv/amd-loader",
+        rv: "bower_components/rv/rv",
+        almond: "bower_components/almond/almond"
+    }
+});
