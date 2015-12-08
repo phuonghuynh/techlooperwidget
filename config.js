16655e3e2a01f4698766af45f79615f50df675a3
Phuong H <phuonghqh@gmail.com>
2015-12-08 15:56:51 +0700

    Add sample salary review data

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 var requirejs = {
-    paths: {
-        text: "bower_components/text/text",
-        jquery: "bower_components/jQuery/dist/jquery",
-        'jquery.scrollbar': "bower_components/jquery.scrollbar/jquery.scrollbar",
-        ractive: "bower_components/ractive/ractive",
-        'amd-loader': "bower_components/rv/amd-loader",
-        rv: "bower_components/rv/rv",
-        almond: "bower_components/almond/almond"
-    }
+  paths: {
+    text: "bower_components/text/text",
+    jquery: "bower_components/jQuery/dist/jquery",
+    ractive: "bower_components/ractive/ractive",
+    'amd-loader': "bower_components/rv/amd-loader",
+    rv: "bower_components/rv/rv",
+    almond: "bower_components/almond/almond"
+  }
 };
--- embed.js
+++ embed.js
@@ -2,6 +2,7 @@ require(["jquery", "app/app"], function ($, app) {
   "use strict";
 
   $(function () {
+    //var div = $(document.body.lastChild).closest('div');
     app.init();
   });
 });
